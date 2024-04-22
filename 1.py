ff6229fe4eb6abcb617879489010e3a9a3158943
kandarp-01 <167208631+kandarp-01@users.noreply.github.com>
2024-04-22 18:56:09 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,52 @@
+import pickle
+from tkinter import messagebox
 #here we will add all modules
-def Validate_user(user_name):
+def Validate_password(password):
     #complete the module
-    pass
-def reset_password():
+    try:
+        file=open("password.bin","rb")
+        original_password=pickle.load(file)
+        if(original_password==password):
+            file.close()
+            return True
+        else:
+            messagebox.showwarning("Sales Analytic","Incorrect Password")
+            file.close()
+            return False
+    except:
+        messagebox.showwarning("Sales Analytic","Data not Found")
+    finally:
+        return False
+        
+def reset_password(password):
     #complete the module
-    pass
-def validate_password_reset():
-    #complete the modue
-    pass
+    def validate_password_reset(password):
+        #complete the modue
+        Upper_case=Num=Spec_char=False
+        for i in password:
+            if(i.isUpper()):
+                Upper_case=True
+            elif(not(i.isalnum()):
+                Spec_char=True
+            elif(i.isdigit()):
+                Num=True
+        if(Upper_case and Num and Spec_char):
+            return True
+        else:
+            messagebox.showwarning("Sales Analytic","Not Valid")
+            
+
+
+
+
+    
+    if(validate_password_reset(password)):
+        file=open("binary.bin","wb")
+        pickle.dump(password,file)
+        file.close()
+        messagebox.showwarning("Sales Analytic","Password Changed")
+        return True
+    else:
+        return False
+        
+
