26560d0d9a221b3d9d8e1477996e480412f86c8d
Aman Singh <2k22.csiot.2212425@gmail.com>
2024-04-22 17:55:51 +0530

    me

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
 #here we will add all modules
+def Validate_user(user_name):
+    #complete the module
+    pass
+def reset_password():
+    #complete the module
+    pass
+def validate_password_reset():
+    #complete the modue
+    pass
