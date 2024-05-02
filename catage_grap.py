a3f942f4f9febbecac210f7e573f11b19566b229
Kandarp Dubey <kandarp1415@gmail.com>
2024-05-02 14:24:09 +0530

    hathi ka kaam

--- catage_grap.py
+++ catage_grap.py
@@ -33,15 +33,43 @@ def back_sh():
     root.destroy()
     os.system("main.py")
 def on_menu_select(value):
-    pass
-
+   # canvas.get_tk_widget().destroy()
+    mycursor.execute("Select date_of_sale,product_sold from boat where category='{}'".format(value))
+    y=[]
+    d=[]
+    for i in mycursor:
+        y.append(i[1])
+        d.append(i[0])
+    import datetime
+    date_list = d
+    x = [date.day for date in date_list]
+    di={}
+    dl=[]
+    for i in range(len(d)):
+        if x[i] in di:
+            di[x[i]]+=y[i]
+        else:
+            di[x[i]]=0
+    n=[]
+    for i in di:
+        n.append(i)
+        count=x.count(i)
+        di[i]=di[i]/count
+        dl.append(di[i])
+        fig = Figure(figsize=(6, 5), dpi=110)
+    plot1 = fig.add_subplot(111)
+    plot1.plot(n, dl, marker='*', color='blue', linestyle='-.')
+    plot1.grid(True)
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
 
 root=Tk()
 root.title("Sales analytic")
 root.geometry('1200x750')
 root.config(bg="red")
 l=Label(root,text="Sales Analytics",font="Airtel 34",bg="blue", fg="white")
-l.place(x=300,y=100)
+l.pack()
 selected_option = StringVar()
 selected_option.set("Categories")
 options =show_categories();
